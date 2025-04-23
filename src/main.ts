628058fa3ff75869fcbcbb3e5cde59e5e2ae8382
Abdomorad <moradabdo127@gmail.com>
2025-04-23 07:21:37 +0200

    eight

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-
-import { config } from 'dotenv';
-import { resolve } from 'path';
-
-config({path:resolve('./config/.env')})
+// api/index.ts
+import createServer from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '../src/app.module';
 
+let server;
 
 async function bootstrap() {
-  const port=process.env.PORT ?? 5000
   const app = await NestFactory.create(AppModule);
-  await app.listen(port,()=>{
-    console.log(port)
-  });
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  app.listen(process.env.PORT ||5000)
+  return createServer(expressApp);
 }
-bootstrap();
+
+export const handler = async (event, context) => {
+  server = server ?? (await bootstrap());
+  return server(event, context);
+};
